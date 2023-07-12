7c0bdd372036584694672f40675166f394fc0e70
andresafcdev <131891757+andresafcdev@users.noreply.github.com>
2023-07-12 15:49:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,43 +2,16 @@ import flask
 
 app = flask.Flask(__name__)
 
-@app.route("/", defaults={"path": ""})
-@app.route("/<path:path>")
-def hello(path):
+@app.route("/")
+def hello():
     headers = flask.request.headers
     query_params = flask.request.args
+    path = flask.request.path
 
-    return """
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Request Information</title>
-        <style>
-            body {
-                font-family: Arial, sans-serif;
-                background-color: #f5f5f5;
-                padding: 20px;
-                margin: 0;
-            }
-            h1 {
-                color: #333333;
-                margin-bottom: 20px;
-            }
-            p {
-                color: #555555;
-                margin-bottom: 10px;
-            }
-        </style>
-    </head>
-    <body>
-        <h1>Request Information:</h1>
-        <p>URI Path: {path}</p>
-        <p>Query Parameters: {query_params}</p>
-        <h1>Request Headers:</h1>
-        {headers}
-    </body>
-    </html>
-    """.format(path=path, query_params=query_params, headers=format_headers(headers))
+    with open("template.html", "r") as file:
+        template = file.read()
+
+    return template.format(path=path, query_params=query_params, headers=format_headers(headers))
 
 def format_headers(headers):
     formatted_headers = ""
